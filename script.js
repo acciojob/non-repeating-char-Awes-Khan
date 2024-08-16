409fa7f0bddd5c6f7dd5324dd06bc055dab93be2
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-16 22:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let uniqueArr = [...(new Set([...str]))];
+    let ans=[];
+    for(let i=0;i<uniqueArr.length; i++){
+        let ind = str.indexOf(uniqueArr[i]);
+        if(str.indexOf(uniqueArr[i], ind+1)==-1)
+            return uniqueArr[i];
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
